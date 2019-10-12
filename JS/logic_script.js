ef21ac7f68954bc3e1dd98184d7ae8535c989715
Antonoiu <Madalin.Antonoiu@merz.com>
2019-10-12 00:32:52 -0400

    2-3 columns with images and text - very awesome! well done

--- JS/logic_script.js
+++ JS/logic_script.js
@@ -31,46 +31,16 @@ function onLoadiframe() {
         //Change src event.srcElement is the same with event.target
         event.srcElement.src="https://picsum.photos/200/300";
         //console.log(event.srcElement);
-        //Remove placeholder of parent div
-        //event.srcElement.parentElement.classList.remove("__placeholder");
 
-        //Remove unncesesarry now parent element with placeholder
-        //event.srcElement.parentNode.parentNode.remove();
+        //Remove placeholder of img
+        event.srcElement.classList.remove("__placeholder");
 
         //need to remvoe placeholder of topmost parent ( topmost div container __placeholder)
-        event.srcElement.parentNode.parentNode.parentNode.parentNode.classList.remove("__placeholder");
+        event.srcElement.parentNode.parentNode.parentNode.classList.remove("__placeholder");
 
-        //make sure it is allowed to run ONLY ONCE (to remove parent div, that`s it)
-        code_happened == 'undefined' ;
-        
-        if (typeof code_happened === 'undefined') {
-          window.code_happened = true;
-          // Your code here.
-          
-          var fragment = document.createDocumentFragment();
-          var element =  event.srcElement.parentNode;
-          
-          while(element.firstChild) {
-              fragment.appendChild(element.firstChild);
-          }
-  
-          element.parentNode.replaceChild(fragment, element);
-        }
-
-        
-        // and it does, but if you add another 2 columns, now code_happened is defined :) So it won't work again
-        
-
-         
-
-
-
-  
-
-
-
-
-
+        //Removes class="" if empty, so on code download you don't have it :)
+        if (event.srcElement.className == "")
+          event.srcElement.removeAttribute('class');
       };
     }
   //
