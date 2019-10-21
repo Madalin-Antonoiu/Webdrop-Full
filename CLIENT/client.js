b04212de2de8b4b1f8ccbae706817212d05b6a35
unknown <antonoiumadalin@gmail.com>
2019-10-21 12:30:44 +0300

    bit more

--- CLIENT/client.js
+++ CLIENT/client.js
@@ -1,4 +1,13 @@
 // Initialization - scripts for the iFrame
 
+// object-fit polyfill run https://mdbootstrap.com/snippets/jquery/pjoter-2-0/934696
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true,
+});
 
 
