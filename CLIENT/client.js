46028fdc7024925ba82275a4976dc194ec5cd0c7
Antonoiu <Madalin.Antonoiu@merz.com>
2019-11-06 18:57:19 -0500

    //## Big Patch ##// - Will get rid of Bootstrap - Will edit css from first class/ id selected - Will have to add CSS/ JS on drop like HTML
    Now i`m only setting the ground navbar test

--- CLIENT/client.js
+++ CLIENT/client.js
@@ -9,3 +9,34 @@
 //jarallax(document.querySelectorAll('.jarallax-keep-img'), {
 //    keepImg: true,
 //});
+
+
+//This is needed for the CSS No BOOTSTRAP NAVBAR
+/*
+
+                        let menuIcon = document.querySelector(".menuIcon");
+                        let nav = document.querySelector(".overlay-menu");
+                
+                        menuIcon.addEventListener("click", () => {
+                            if (nav.style.transform != "translateX(0%)") {
+                                nav.style.transform = "translateX(0%)";
+                                nav.style.transition = "transform 0.2s ease-out";
+                            } else { 
+                                nav.style.transform = "translateX(-100%)";
+                                nav.style.transition = "transform 0.2s ease-out";
+                            }
+                        });
+                
+                
+                        // Toggle Menu Icon ========================================
+                        let toggleIcon = document.querySelector(".menuIcon");
+                
+                        toggleIcon.addEventListener("click", () => {
+                            if (toggleIcon.className != "menuIcon toggle") {
+                                toggleIcon.className += " toggle";
+                            } else {
+                                toggleIcon.className = "menuIcon";
+                            }
+                        });
+*/
+                
